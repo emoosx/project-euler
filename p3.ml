10c56b9e91c07e456a64b4d9e63f1e25a350e13c
Kaung Htet Zaw <emoosx@gmail.com>
2015-01-23 17:11:42 +0800

    Finish problem 3

--- p3.ml
+++ p3.ml
@@ -3,38 +3,16 @@
  * 
  * What is the largest prime factor of the number 600851475143 ?
  *)
+let factors n =
+  let rec aux d n =
+    if n = 1 then [] else
+      if n mod d = 0 then d :: aux d (n/d) else aux (d+1) n
+  in aux 2 n;;
 
-let range n =
-  let rec aux n acc =
-    if n < 2 then acc
-    else aux (pred n) (n :: acc)
-  in aux n []
-;;
+let rec last = function
+  | [] -> None
+  | [x] -> Some x
+  | _ :: t -> last t;;
 
-let remove l n =
-  let rec aux l = match l with
-  | [] -> []
-  | hd::tl -> if hd <> n then hd::(aux tl) else aux tl
-  in aux l ;;
+let _ = last (factors 600851475143);;
 
-let fold_iter f a b init step =
-  let rec aux acc i =
-    if i > b then acc else aux (f acc i) (i + step)
-  in aux init a ;;
-
-let primes n =
-  let numbers = (range n) in
-  let limit = truncate(sqrt(float n)) in
-  fold_iter (fun li x ->
-    if List.mem x li then (fold_iter remove (x*x) n li x) else li)
-  2 (limit) numbers 1
-;;
-
-let largest_prime_factor n = 
-  let limit = truncate(sqrt(float n)) in
-  let primes = primes n in
-  let rec aux acc n = function
-    | [] -> acc
-    | hd::tl -> if (n mod hd) = 0 then aux hd (n/hd) tl else aux acc n tl
-  in aux 1 n primes
-;;
