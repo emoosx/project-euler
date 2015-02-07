520a20c105586a4985c297bd56e16bc5e7ccb404
Kaung Htet Zaw <emoosx@gmail.com>
2015-02-07 22:56:56 +0800

    Finish problem 45 in python

---
+++ python/p45.py
@@ -0,0 +1,32 @@
+from heapq import merge
+import itertools
+
+
+def triangulars():
+    n = 1
+    while True:
+        yield (n * (n + 1)) / 2
+        n += 1
+
+def pentagonals():
+    n = 1
+    while True:
+        yield (n * (3 * n - 1)) / 2
+        n += 1
+
+def hexagonals():
+    n = 1
+    while True:
+        yield n * (2 * n - 1)
+        n += 1
+
+grouped =  itertools.groupby(merge(triangulars(), pentagonals(), hexagonals()))
+
+count = 0
+for key, group in grouped:
+    if count == 3:
+        break
+    g = list(group)
+    if len(g) == 3:
+        print g
+        count += 1
